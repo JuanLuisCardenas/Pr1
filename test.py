787fe2bef762a0865f092ce58a6247fec729af3e
RuuKw <john117.mine@gmail.com>
2019-08-14 23:02:27 -0500

    User character script.

---
+++ test.py
@@ -0,0 +1,14 @@
+class Tester(object):
+    def __init__(self, tipo, nombre, hp, attak, defns):
+        self.tipo = tipo
+        self.nombre = nombre
+        self.hp = hp
+        self.attak = attak
+        self.defns = defns
+
+    def info(self):
+        print('Tipo: {}'.format(self.tipo))
+    
+
+    
+
